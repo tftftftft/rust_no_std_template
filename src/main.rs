c551f74c232518c2e064be0a72f2e5a930bf3104
adscam <43244566+tftftftft@users.noreply.github.com>
2023-07-12 16:58:34 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ static ALLOCATOR: LibcAlloc = LibcAlloc;
 static mut value: i32 = 0;
 #[no_mangle]
 pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    // Since we are passing a C string the final null character is mandatory.
     const HELLO: &'static str = "Hello, world!\n\0";
     unsafe {
         libc::printf(b"%s\0".as_ptr() as *const _, HELLO.as_ptr() as *const _);
